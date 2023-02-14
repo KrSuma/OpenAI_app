23172ca0b32f41b4d640b29af7a1f9789e4a8232
jslee <jaesun90@gmail.com>
2023-02-14 17:06:10 +0900

    adding some tests

--- OpenAi_test.py
+++ OpenAi_test.py
@@ -1,4 +1,13 @@
 from OpenAI import *
 
 
+authenticate()
+
+# model_list()
+
+# model_retrieve("text-davinci-003")
+
+# completion(default_model, "Write a tagline for an ice cream shop.")
+
+# generate_image("cat in van gogh art style")
 
